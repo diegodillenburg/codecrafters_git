5e5ce35a016f287feb3218829d7407930d1564d9
Diego Dillenburg <diegodillenburg+github@gmail.com>
2024-11-28 19:16:48 -0300

    refactor commands::init

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,10 +1,23 @@
 use std::fs;
+use std::env;
 
 pub fn init() {
-    fs::create_dir(".git").unwrap();
-    fs::create_dir(".git/objects").unwrap();
-    fs::create_dir(".git/refs").unwrap();
-    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+    let git_dir = if env::var("GIT_DIR").is_ok() {
+        Some(env::var("GIT_DIR").unwrap())
+    } else {
+        None
+    };
+
+    let path = if git_dir.is_none() {
+        ".git".to_string()
+    } else {
+        format!("{}/.git", git_dir.unwrap())
+    };
+
+    fs::create_dir(&path).unwrap();
+    fs::create_dir(format!("{}/objects", &path)).unwrap();
+    fs::create_dir(format!("{}/refs", &path)).unwrap();
+    fs::write(format!("{}/HEAD", &path), "ref: refs/heads/main\n").unwrap();
 
     println!("Initialized git directory")
 }
